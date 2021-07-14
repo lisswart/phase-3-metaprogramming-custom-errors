f55da8a02001eac654ece5f57c6b7db11d626643
lisswart <arnoudlisa@gmail.com>
2021-07-14 02:03:15 -0400

    done

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,3 +1,9 @@
+class PartnerError < StandardError
+    def message
+      "you must give the get_married method an argument of an instance of the person class!"
+    end
+end
+
 class Person
   attr_accessor :partner, :name
 
@@ -7,7 +13,15 @@ class Person
 
   def get_married(person)
     self.partner = person
-    person.partner = self
+    if person.class != Person
+      begin
+        raise PartnerError
+      rescue PartnerError => error
+        puts error.message
+      end
+    else
+      person.partner = self
+    end    
   end
 
 end
